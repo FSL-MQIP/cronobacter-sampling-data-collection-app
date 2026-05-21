bdb7c358462342f090f1453265bf2d65b7ff258d
yjung354 <112415947+yjung354@users.noreply.github.com>
2026-05-21 15:19:21 -0400

    chore: bump SW cache to v4 + skipWaiting/clients.claim
    So devices actually pick up new files on the next load instead of being
    stuck on the previously cached shell.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'crono-v3';
+const CACHE = 'crono-v4';
 const SHELL = [
   './', './index.html', './css/styles.css',
   './js/app.js', './js/sample-id.js', './js/session.js', './js/storage.js',
@@ -7,14 +7,17 @@ const SHELL = [
   './manifest.json',
 ];
 
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)))
-);
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+});
 
 self.addEventListener('activate', e =>
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ))
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  )
 );
 
 self.addEventListener('fetch', e => {
